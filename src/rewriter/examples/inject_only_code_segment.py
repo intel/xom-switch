a989641ddb9ca05de106b917c77c9cb4259471bf
Zhang, Mingwei <mingwei.zhang@intel.com>
2018-03-26 16:21:57 -0700

    update instrumentor code

--- src/rewriter/examples/inject_only_code_segment.py
+++ src/rewriter/examples/inject_only_code_segment.py
@@ -18,29 +18,12 @@ class code_injector(instrumentor):
 
     def compile_inject_instrument_file(self, asmfile, objfile, injectfile,
                                        secname, align):
-        self.extract_data(objfile, '.text', injectfile, padalignpage=True)
+        self.extract_data(objfile, '.text', injectfile, padalignpage=False)
         self.add_instrumentation_data(self.get_current_file(), injectfile,
                                       secname, align)
     def patch_relocation(self, binname):
         pass
 
-class rodata_injector(instrumentor):
-    def __init__(self, name, filename, secname, objfile):
-        super(rodata_injector, self).__init__(name, filename, secname)
-        self.obj_file = objfile
-        self.inject_file = self.get_tempfile()
-        self.mapping = dict()
-
-    def generate_instrumentation(self, asmfile):
-        pass
-
-    def compile_inject_instrument_file(self, asmfile, objfile, injectfile,
-                                       secname, align):
-        self.extract_data(objfile, '.rodata', injectfile)
-        self.add_instrumentation_data(self.get_current_file(), injectfile,
-                                      secname, align)
-    def patch_relocation(self, binname):
-        pass
 
 def main():
     parser = argparse.ArgumentParser()
@@ -54,12 +37,7 @@ def main():
     scheduler = instrument_scheduler()
     codeinjector = code_injector('instrumented_code', args.file,
                                '.instrumented_code', args.inject)
-    rodatainjector = rodata_injector('instrumented_data', args.file,
-                               '.instrumented_data', args.inject)
-    codeinjector.prefer_inject_new_segment = 0
-    rodatainjector.prefer_inject_new_segment = 0
     scheduler.register_instrumentor(codeinjector)
-    scheduler.register_instrumentor(rodatainjector)
     scheduler.perform_instrumentation()
     print scheduler.get_current_file()
     if(args.output == None):
